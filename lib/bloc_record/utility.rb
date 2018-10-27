f68dc8d2e8af4ae5edfcd9a497b648ff8afd0597
Steeve Postemus <spostemus@gmail.com>
2018-10-27 18:33:50 -0500

    finished select methods

--- lib/bloc_record/utility.rb
+++ lib/bloc_record/utility.rb
@@ -1,6 +1,7 @@
 module BlocRecord
   module Utility
     extend self # extend allows us to add module methods as class methods
+
     def underscore(camel_cased_word)
       string = camel_cased_word.gsub(/::/, '/')
       string.gsub!(/([A-Z]+)([A-Z][a-z])/,'\1_\2')
@@ -22,7 +23,8 @@ module BlocRecord
     end
 
     def convert_keys(options)
-      # converts all the keys to string keys. Use either strings or symbols as hash keys.
+      # converts all the keys to string keys.
+      # Allows us to use either strings or symbols as hash keys.
       options.keys.each {|k| options[k.to_s] = options.delete(k) if k.kind_of?(Symbol)}
       options
       # delete method returns the value of deleted key.
@@ -35,7 +37,7 @@ module BlocRecord
     end
 
     def reload_obj(dirty_obj)
-      persisted_obj = dirty_obj.class.find(dirty_obj.id)
+      persisted_obj = dirty_obj.class.find_one(dirty_obj.id)
       #  takes an object, finds its database record using the find method in the Selection module.
       dirty_obj.instance_variables.each do |instance_variable|
         dirty_obj.instance_variable_set(instance_variable,  persisted_obj.instance_variable_get(instance_variable))
