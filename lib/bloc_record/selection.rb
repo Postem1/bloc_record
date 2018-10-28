cfe22f547f201ddf23ee33083d98ec141e01c4cb
Steeve Postemus <spostemus@gmail.com>
2018-10-27 19:45:51 -0500

    finished select assignment

--- lib/bloc_record/selection.rb
+++ lib/bloc_record/selection.rb
@@ -2,6 +2,8 @@ require 'sqlite3'
 
 module Selection
   def find(*ids)
+    # todo :implement rejected id array
+    ids.select { |id| id.is_a? Integer && id > 0 }
 
     if ids.length == 1
       find_one(ids.first)
@@ -16,34 +18,45 @@ module Selection
   end
 
   def find_one(id)
-    row = connection.get_first_row <<-SQL
-      SELECT #{columns.join ","} FROM #{table}
-      WHERE id = #{id};
-    SQL
+    if id.is_a? Integer && id > 0
+      row = connection.get_first_row <<-SQL
+        SELECT #{columns.join ","} FROM #{table}
+        WHERE id = #{id};
+      SQL
       # get_first_row is method from sqlite3 for obtaining the first row of a result set, and discarding all others. It is otherwise identical to #execute.
       init_object_from_row(row)
+    else
+      raise ArgumentError.new("Please enter a valid ID...must be a positive whole number.")
+    end
   end
 
   def find_by(attribute, value)
-    rows = connection.execute <<-SQL
-      SELECT #{columns.join ","} FROM #{table}
-      WHERE #{attribute} = #{BlocRecord::Utility.sql_strings(value)};
-    SQL
-
-    rows_to_array(rows)
-  end
-
-  def take(num=1)
-    if num > 1
+    if columns.include?(attribute)
       rows = connection.execute <<-SQL
         SELECT #{columns.join ","} FROM #{table}
-        ORDER BY random()
-        LIMIT #{num};
+        WHERE #{attribute} = #{BlocRecord::Utility.sql_strings(value)};
       SQL
 
       rows_to_array(rows)
     else
-      take_one
+      raise ArgumentError.new("#{attribute} is not a valid attribute. Please try again.")
+    end
+  end
+
+  def take(num=1)
+    if num.is_a? Integer && num > 0
+      if num > 1
+        rows = connection.execute <<-SQL
+          SELECT #{columns.join ","} FROM #{table}
+          ORDER BY random()
+          LIMIT #{num};
+        SQL
+
+        rows_to_array(rows)
+      else
+        take_one
+      end
+      raise ArgumentError.new("That is not a valid ID...must be a positive whole number.")
     end
   end
 
@@ -81,6 +94,38 @@ module Selection
     SQL
 
     rows_to_array(rows)
+  end
+
+  def find_each(options = {})
+    start = options[:start] || 0
+    batch_size = options[:batch_size] || 100
+
+    rows = connection.execute <<-SQL
+      SELECT #{columns.join ","} FROM #{table}
+      LIMIT #{batch_size} OFFSET #{start};
+    SQL
+
+    rows.each do |row|
+      yield init_object_from_row(row)
+    end
+  end
+
+  def find_in_batches(options = {})
+    start = options[:start] || 0
+    batch_size = options[:batch_size] || 100
+
+    rows = connection.execute <<-SQL
+      SELECT #{columns.join ","} FROM #{table}
+      LIMIT #{batch_size} OFFSET #{start};
+    SQL
+
+    rows_to_array(rows)
+  end
+
+  def method_missing(method, *args)
+    attribute = method.to_s
+    attribute.slice!("find_by_")
+    find_by(attribute, args[0])
   end
 
 
