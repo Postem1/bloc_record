1742c6fd32e572afe22eb17efe8b7aad0389df34
Steeve Postemus <spostemus@gmail.com>
2018-11-11 07:40:05 -0600

    finished where order checkpoint

--- lib/bloc_record/selection.rb
+++ lib/bloc_record/selection.rb
@@ -128,6 +128,68 @@ module Selection
     find_by(attribute, args[0])
   end
 
+  def where(*args)
+    if args.count > 1
+      expression = args.shift
+      params = args
+    else
+      case args.first
+      when String
+        expression = args.first
+      when Hash
+         expression_hash = BlocRecord::Utility.convert_keys(args.first)
+         expression = expression_hash.map {|key, value| "#{key}=#{BlocRecord::Utility.sql_strings(value)}"}.join(" and ")
+      end
+    end
+
+    sql = <<-SQL
+      SELECT #{columns.join ","} FROM #{table}
+      WHERE #{expression};
+    SQL
+
+    rows = connection.execute(sql, params)
+    rows_to_array(rows)
+  end
+
+  def order(*args)
+    if args.count > 1
+      order = args.join(",")
+    else
+      order = args.first.to_s
+    end
+    # If the order local variable is a Symbol, to_s will convert it to a string
+
+    rows = connection.execute <<-SQL
+      SELECT * FROM #{table}
+      ORDER BY #{order};
+    SQL
+
+    rows_to_array(rows)
+  end
+
+  def join(*args)
+    if args.count > 1
+      joins = args.map { |arg| "INNER JOIN #{arg} ON #{arg}.#{table}_id = #{table}.id"}.join(" ")
+      rows = connection.execute <<-SQL
+        SELECT * FROM #{table} #{joins}
+      SQL
+    else
+      case args.first
+      when String
+        rows = connection.execute <<-SQL
+          SELECT * FROM #{table} #{BlocRecord::Utility.sql_strings(args.first)};
+        SQL
+      when Symbol
+        rows = connection.execute <<-SQL
+          SELECT * FROM #{table}
+          INNER JOIN #{args.first} ON #{args.first}.#{table}_id = #{table}.id
+        SQL
+      end
+    end
+
+    rows_to_array(rows)
+  end
+
 
   private
 
@@ -143,3 +205,7 @@ module Selection
   end
 
 end
+
+# a = [ 4, 5, 6 ]
+# b = [ 7, 8, 9 ]
+# [1,2,3].zip(a, b)      #=> [[1, 4, 7], [2, 5, 8], [3, 6, 9]]
