c94490457f7411010f2cb4baaf760b3ff45c461a
Steeve Postemus <spostemus@gmail.com>
2018-11-18 10:37:38 -0600

    finished destroy checkpoint

--- lib/bloc_record/persistence.rb
+++ lib/bloc_record/persistence.rb
@@ -44,6 +44,10 @@ module Persistence
     update_attribute(attribute, args[0])
   end
 
+  def destroy
+    self.class.destroy(self.id)
+  end
+
 
   module ClassMethods
     def create(attrs)
@@ -94,6 +98,38 @@ module Persistence
 
     def update_all(updates)
        update(nil, updates)
+    end
+
+    def destroy(*id)
+      if id.length > 1
+        where_clause = "WHERE id IN (#{id.join(",")});"
+      else
+        where_clause = "WHERE id = #{id.first};"
+      end
+
+      connection.execute <<-SQL
+        DELETE FROM #{table} #{where_clause}
+      SQL
+
+      true
+    end
+
+    def destroy_all(conditions_hash=nil)
+      if conditions_hash && !conditions_hash.empty?
+        conditions_hash = BlocRecord::Utility.convert_keys(conditions_hash)
+        conditions = conditions_hash.map {|key, value| "#{key}=#{BlocRecord::Utility.sql_strings(value)}"}.join(" and ")
+
+        connection.execute <<-SQL
+          DELETE FROM #{table}
+          WHERE #{conditions};
+       SQL
+      else
+        connection.execute <<-SQL
+          DELETE FROM #{table}
+        SQL
+      end
+
+      true
     end
 
   end
