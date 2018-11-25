e4ac1c3278abba910d4c6d46be79089782fd67ca
Steeve Postemus <spostemus@gmail.com>
2018-11-25 07:47:28 -0500

    added pg and has_one

--- lib/bloc_record.rb
+++ lib/bloc_record.rb
@@ -1,9 +1,14 @@
 module BlocRecord
-  def self.connect_to(filename)
+  def self.connect_to(filename, server)
     @database_filename = filename
+    @database_server = server
   end
 
   def self.database_filename
     @database_filename
+  end
+
+  def self.database_server
+    @database_server
   end
 end
--- lib/bloc_record/associations.rb
+++ lib/bloc_record/associations.rb
@@ -1,4 +1,5 @@
 require 'sqlite3'
+require 'pg'
 require 'active_support/inflector'
 
 module Associations
@@ -19,6 +20,22 @@ module Associations
       end
 
       collection
+    end
+  end
+
+  def has_one(association)
+    define_method(association) do
+      row = self.class.connection.get_first_row <<-SQL
+        SELECT * FROM #{association.to_s.singularize}
+        WHERE #{self.class.table}_id = #{self.id};
+      SQL
+
+      class_name = association.to_s.classify.constantize
+
+      if row
+        data = Hash[class_name.columns.zip(row)]
+        class_name.new(data)
+      end
     end
   end
 
